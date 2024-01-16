6c44e60d16c2c6896ed9ed973daa086a60e993a4
DerDawo <71173937+DerDawo@users.noreply.github.com>
2024-01-16 09:44:21 +0000

    caroussel added

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.addEventListener("scroll",fadeInOnScroll)
+window.addEventListener("load", init)
+
+function init(){
+  document.getElementById("previous-caroussel-item").addEventListener("click",fadeInOnDisplay)
+  document.getElementById("next-caroussel-item").addEventListener("click",fadeInOnDisplay)
+  for(let i = 0; i < document.getElementsByClassName("caroussel-item-dot").length; i++){
+    document.getElementsByClassName("caroussel-item-dot")[i].addEventListener("click",fadeInOnDisplay)
+  }
+  document.addEventListener("scroll",fadeInOnScroll)
+  document.getElementById("open_mobile_nav").addEventListener("click",openMobileNav)
+  document.getElementById("close_mobile_nav").addEventListener("click",closeMobileNav)
+  for(let i = 0; i < document.getElementsByClassName("mobile_nav_option").length; i++){
+    document.getElementsByClassName("mobile_nav_option")[i].addEventListener("click",closeMobileNav)
+  }
+ 
+}
 
 function fadeInOnScroll(){
     const pageTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
@@ -16,15 +31,29 @@ function fadeInOnScroll(){
     }
 }
 
-
-document.getElementById("open_mobile_nav").addEventListener("click",openMobileNav)
-document.getElementById("close_mobile_nav").addEventListener("click",closeMobileNav)
-for(let i = 0; i < document.getElementsByClassName("mobile_nav_option").length; i++){
-  document.getElementsByClassName("mobile_nav_option")[i].addEventListener("click",closeMobileNav)
-}
 function openMobileNav(){
   document.getElementsByClassName("mobile_nav")[0].style.transform = "translateX(0%)";
 }
 function closeMobileNav(){
   document.getElementsByClassName("mobile_nav")[0].style.transform = "translateX(100%)";
+}
+
+function fadeInOnDisplay(){
+  const restaurant_slide = document.getElementsByClassName("restaurant_slide");
+  const fade_ins_display = document.getElementsByClassName("fade-in-caroussel");
+
+  for (var i = 0; i < fade_ins_display.length; i++) {
+    const fade_in = fade_ins_display[i];
+    fade_in.classList.remove("visible-caroussel-content");
+  }
+
+  for (const slide of restaurant_slide){
+    if (getComputedStyle(slide, null).display !== "none"){
+      const fade_ins_display_show = slide.getElementsByClassName("fade-in-caroussel")
+      for (var i = 0; i < fade_ins_display_show.length; i++) {
+        fade_ins_display_show[i].classList.add("visible-caroussel-content");
+      }
+    }
+  }
+
 }
